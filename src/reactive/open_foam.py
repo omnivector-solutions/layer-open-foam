5984f8e841d21b796a76c38c2e6a97ae260c066d
jamesbeedy <jamesbeedy@gmail.com>
2018-11-24 04:29:46 -0800

    support bionic

--- src/reactive/open_foam.py
+++ src/reactive/open_foam.py
@@ -4,11 +4,11 @@ from charmhelpers.core.hookenv import status_set
 from charmhelpers.core.hookenv import application_version_set
 
 
-@when('apt.installed.openfoam5')
+@when('apt.installed.openfoam6')
 @when_not('openfoam.version.set')
 def set_version_status():
     """Set version and status
     """
-    application_version_set(get_upstream_version('openfoam5'))
-    status_set('active', 'openFOAM available')
+    application_version_set(get_upstream_version('openfoam6'))
+    status_set('active', 'OpenFOAM available')
     set_flag('openfoam.version.set')
